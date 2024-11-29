9c46781ef7ea5460f577bb3bd4c7551acdb4dcab
Ryan Seddon <seddon.ryan@gmail.com>
2024-11-29 14:44:01 +1100

    Make it offline capable

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        // ffmpeg-core.wasm is ~32MB
+        maximumFileSizeToCacheInBytes: 32.1 * 1024 ** 2,
+      },
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
